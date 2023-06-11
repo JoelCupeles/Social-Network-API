19a82f76901dab6aee88176240369d7e8131f958
JoelCupeles <joel.cupeles1998@gmail.com>
2023-06-10 19:01:34 -0500

    Controllers

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router
+  .route('/')
+  .get(getAllThoughts)
+  .post(createThought);
+
+// /api/thoughts/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction);
+
+
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes');
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+if (process.env.NODE_ENV === "production") -+
+  app.use(express.static("client/build"));
+
+app.use('/api', apiRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
