8e6f2b7fd79f9f4c1e14318a24ee993a1c764221
JoelCupeles <joel.cupeles1998@gmail.com>
2023-06-11 11:57:05 -0500

    Debugging and fixes to index.js files

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,19 +1,13 @@
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 
 const userController = {
-  // get all users
   getAllUsers(req, res) {
     User.find({})
-    .populate({
-      path: 'thoughts',
-      select: '-__v'
-    })
-    .select('-__v')
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
 
   // get one user by id
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes');
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") -+
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+}
 
-app.use('/api', apiRoutes);
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/socialnetwork", { 
+   useNewUrlParser: true, 
+   useUnifiedTopology: true 
 });
 
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.listen(PORT, function() {
+   console.log(`🌍 Now listening on localhost:${PORT}`);
+});
